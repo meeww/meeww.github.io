f44871da1e16e0a930469854be39b1d5241ecb83
meeww <40210770+meeww@users.noreply.github.com>
2021-06-11 13:54:20 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -544,24 +544,13 @@ function touchStarted(){
   
   }
   
-  if(touches[0].x<menuSize&touches[0].x>height/10&touches[0].y>height-height/10&touches[0].y<height){
+  if(touches[0].x<menuSize&touches[0].x>height/10&touches[0].y>height-height/10&touches[0].x<height){
 
     
-    onDeviceReady();
+     let outputImage = get(menuSize,0,width,height);
+    save(outputImage,"output.png")
   
   }
-}
-function onDeviceReady()
-{
-	window.canvas2ImagePlugin.saveImageDataToLibrary(
-        function(msg){
-            console.log(msg);
-        },
-        function(err){
-            console.log(err);
-        },
-        document.getElementById('myCanvas')
-    );
 }
 function touchEnded(){
       
