b8c6042f28e78113cde78224a54b9497816ed2d1
meeww <40210770+meeww@users.noreply.github.com>
2021-06-11 13:20:57 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -542,11 +542,6 @@ function mousePressed(){
     pause = toggle(pause);
   
   }
-  function touchStarted(){
-	//  if(touches[0].x<height/10&touches[0].y>height-height/10&touches[0].y<height){
-		  pause = toggle(pause);
-	//	  }
-  }
   if(mouseX<menuSize&mouseX>height/10&mouseY>height-height/10&mouseY<height){
 
     
